d2c2552daf925fdb8d12f81c45335a66ae3eca9b
afeiship <1290657123@qq.com>
2024-04-09 10:39:40 +0800

    fix: missing gulp headers

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const { NxScripts, CleanRegistry } = require('@jswork/gulp-registry');
 
 const task1 = new CleanRegistry();
-const task2 = new NxScripts({ name: 'qs', classify: false });
+const task2 = new NxScripts({ classify: false });
 
 [task1, task2].forEach(gulp.registry);
 
